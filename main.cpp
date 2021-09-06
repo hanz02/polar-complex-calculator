e16d16d7226d45365ee6a6fba046c42dc96eadae
hanzhe2002 <khawhanzhe@gmail.com>
2021-09-07 02:22:12 +0800

    polar coord equality algorithm finished (further test cases needed)

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+ComplexNumber cn1;
+PolarCoord pc1(-2, 210);
+
 int main()
 {
 
@@ -13,7 +16,8 @@ int main()
 
     char user_opt = '0';
 
-    do {
+    do
+    {
         cout << "\n\n====== MAIN MENU ======\n";
         cout << "\nChoose an operation to perform below (Choose 1 to 3)\n\n";
         cout << "1. Polar Coordinates & Complex Numbers Conversion \n";
@@ -25,24 +29,28 @@ int main()
 
         switch(user_opt)
         {
-            // conversion
-            case '1':
-                cout << "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-                conversionMenu(user_opt);
-                break;
+        // conversion
+        case '1':
+            cout << "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+            conversionMenu(user_opt);
+            break;
 
-            case '2':
+        case '2':
+        {
+            PolarCoord pc2(2, 30);
+            pc2.is_equal(pc1) ? cout << "yay" : cout << "bruh";
+            break;
+        }
 
-                break;
-            // exit program
-            case '4':
-                cout << "\n====== Thank You for using the calculator ====== \n";
-                break;
-            default:
-                cout << "\nIncorrect Option Specified (Please choose option 1 to 2) \n";
-                system("pause");
-                cout << "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-                break;
+        // exit program
+        case '3':
+            cout << "\n====== Thank You for using the calculator ====== \n";
+            break;
+        default:
+            cout << "\nIncorrect Option Specified (Please choose option 1 to 2) \n";
+            system("pause");
+            cout << "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+            break;
         }
 
     }
